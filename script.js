a7c44f5fe41360dc760afcd99b0aa6539c6628f5
keelbrissett <keelbrissett02@gmail.com>
2025-04-07 20:37:42 -0400

    Finished

--- script.js
+++ script.js
@@ -26,14 +26,26 @@ const questions = [
         { text: "Computer Style Sheets", correct: false },
         { text: "Creative Style Syntax", correct: false }
       ]
+    },
+    {
+      question: "What is the capital of Jamaica?",
+      answers: [
+        { text: "Queenstown", correct: false },
+        { text: "Kingstown", correct: false },
+        { text: "Kingston", correct: true },
+        { text: "Kingsville", correct: false }
+      ]
     }
   ];
   
   // 2. How do we know what id to search for when using document.getElementById()? Where are the following ids specified in index.html? 
-  // 
+  // Each div and/or element in an html have a field for an id so you can uniquely reference them. Once an id is used once, it cannot
+  // used again. Thus, for usages like "document.getElementById", you can get the value that is associated with that id
   const questionElement = document.getElementById("question");
   const answerButtonsElement = document.getElementById("answer-buttons");
   const nextButton = document.getElementById("next-btn");
+  const hintButton = document.getElementById("hint-btn");
+
   
   let currentQuestionIndex = 0;
   let score = 0;
@@ -52,7 +64,9 @@ const questions = [
   
     currentQuestion.answers.forEach(answer => {
       // 3. Why are these HTML elements being created dynamically in the JS file, while other page elements are defined statically in the HTML file?
-      // 
+      // These variables will be different based on the current question that is being answered. In our case, the answers and infromation associated 
+      // with the questions are different, thus you need these to be assigned dynamically based on the question being answered. Additionally, each 
+      // question can have a different amount of answers
       const button = document.createElement("button");
       button.textContent = answer.text;
       button.classList.add("btn");
@@ -61,15 +75,18 @@ const questions = [
       }
       button.addEventListener("click", selectAnswer);
       // 4. What is the line below doing? 
-      // 
+      // This makes the next button appear below the question when the user provides an answer
       answerButtonsElement.appendChild(button);
     });
   }
   
   function resetState() {
     nextButton.style.display = "none";
+    hintButton.style.display = "block";
+    hintButton.disabled = false;
     answerButtonsElement.innerHTML = "";
   }
+  
   
   function selectAnswer(e) {
     const selectedBtn = e.target;
@@ -88,7 +105,7 @@ const questions = [
       button.disabled = true;
     });
     // 5. Why is it important to change the display styling rule for the "Next" button to "block" here? What would happen if you did not have this line?
-    // 
+    // Originally the display is set to none, so here you are changing the display so the next button actually shows up 
     nextButton.style.display = "block";
   }
   
@@ -109,7 +126,10 @@ const questions = [
   }
   
   // 6. Summarize in your own words what you think this block of code is doing. 
-  // 
+  // The below code is providing logic for the "next button". Essentially, it is checking if we have more question by checking the our current question "index"
+  // with the total length of the questions array. If we have not reached the end of the array, i.e. we have more questions, then we will go to the next question
+  // by leveraging the handleNextButton() method. Otherwise, we are at the end of the array meaning we have no more questions left so we restart the quiz by using
+  // the startQuiz() method
   nextButton.addEventListener("click", () => { 
     if (currentQuestionIndex < questions.length) {
       handleNextButton();
@@ -117,6 +137,19 @@ const questions = [
       startQuiz();
     }
   });
+
+  hintButton.addEventListener("click", () => {
+    const buttons = Array.from(answerButtonsElement.children);
+    const wrongButtons = buttons.filter(btn => btn.dataset.correct !== "true" && !btn.classList.contains("wrong"));
+    
+    if (wrongButtons.length > 0) {
+      const randomWrong = wrongButtons[Math.floor(Math.random() * wrongButtons.length)];
+      randomWrong.classList.add("wrong");
+      randomWrong.disabled = true;
+      hintButton.disabled = true;
+    }
+  });
+  
   
   startQuiz();
   
